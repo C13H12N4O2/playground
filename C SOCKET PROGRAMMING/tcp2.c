edde01997fe5cab5dfaaf253bc8a1574d62cb0d3
Psyduck <31785341+C13H12N4O2@users.noreply.github.com>
2024-03-14 16:52:31 +0900

    Create tcp2.c

---
+++ C SOCKET PROGRAMMING/tcp2.c
@@ -0,0 +1,160 @@
+#include <time.h>
+#include <poll.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT        7000
+#define TIMEOUT     5000
+#define SIZE_BUF    48
+#define CNT_POLL    2
+
+#define RTN_ERROR       -1
+#define RTN_SUCCESS      0
+#define RTN_TIMEOUT      0 
+
+#define EXIT(msg)   do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+typedef struct
+{
+    char sStr[SIZE_BUF];
+} Packet;
+
+const char *sStr[] = { "Apple",  "LG",      "Telsa",  "Discord", "Google", 
+                               "Amazon", "Twitter", "SpaceX", "Samsung", "Hyundai", };
+
+int main();
+int fn_ListenTCPSocket();
+int fn_Poll(struct pollfd *, int *);
+int fn_SendPacket(struct pollfd *, int *);
+
+int main()
+{
+    struct pollfd       tPollFds[CNT_POLL];
+    int                 nSockFd;
+    int                 nRtn;
+    int                 nIdx;
+                                   
+    nSockFd = fn_ListenTCPSocket();
+    
+    srand(time(NULL));
+    
+    signal(SIGPIPE, SIG_IGN);
+    
+    tPollFds[0].fd      = nSockFd;
+    tPollFds[0].events  = POLLIN;
+    
+    nRtn = fn_Poll(tPollFds, &nSockFd);
+    
+    close(nSockFd);
+    
+    return 0;
+}
+
+int fn_ListenTCPSocket()
+{
+    struct sockaddr_in  tSockaddr;
+    int                 nSockFd;
+    
+    if ((nSockFd = socket(AF_INET, SOCK_STREAM, 0)) == RTN_ERROR)
+        return RTN_ERROR;
+        
+    memset((struct sockaddr_in *) &tSockaddr, 0x00, sizeof(tSockaddr));
+    tSockaddr.sin_family        = AF_INET;
+    tSockaddr.sin_addr.s_addr   = INADDR_ANY;
+    tSockaddr.sin_port          = htons(PORT);
+    
+    if (bind(nSockFd, (struct sockaddr *) &tSockaddr, sizeof(tSockaddr)) < 0)
+        return RTN_ERROR;
+        
+    if (listen(nSockFd, 0) < 0)
+        return RTN_ERROR;
+        
+    return (nSockFd);
+}
+
+int fn_Poll(struct pollfd *tPollFds, int *nSockFd)
+{
+    struct sockaddr_in  tAcptAddr;
+    socklen_t           nAddrLen = sizeof(tAcptAddr);
+    int                 nAcptSockFd;
+    int                 nRtn;
+    
+    while (1)
+    {
+        nRtn = poll(tPollFds, CNT_POLL, TIMEOUT);
+        
+        if      (nRtn == RTN_ERROR)     break;
+        else if (nRtn == RTN_TIMEOUT)   printf("TIMEOUT\n");
+        else
+        {
+            memset((struct sockaddr *) &tAcptAddr, 0x00, sizeof(tAcptAddr));
+    
+            if (tPollFds[0].revents & POLLHUP)
+            {
+                tPollFds[0].revents = 0;
+                break;
+            }
+            else if (tPollFds[0].revents & POLLIN)
+            {
+                tPollFds[0].revents = 0;
+                
+                if ((nAcptSockFd = accept(*nSockFd, (struct sockaddr *) &tAcptAddr, &nAddrLen)) == RTN_ERROR)
+                    break;
+                    
+                tPollFds[1].fd      = nAcptSockFd;
+                tPollFds[1].events  = POLLIN;
+                    
+                fn_SendPacket(tPollFds, &nAcptSockFd);
+            }
+        }
+    }
+    
+    return RTN_ERROR;
+}
+
+int fn_SendPacket(struct pollfd *tPollFds, int *nSockFd)
+{
+    Packet              tPacket;
+    int                 iIdx;
+    int                 nRtn;
+    
+    while (1)
+    {
+        nRtn = poll(tPollFds, CNT_POLL, TIMEOUT);
+        
+        if      (nRtn == RTN_ERROR)     return RTN_ERROR;
+        else if (nRtn == RTN_TIMEOUT)   printf("TIMEOUT\n");
+        else
+        {
+            if (tPollFds[0].revents & POLLHUP)
+            {
+                tPollFds[0].revents = 0;
+                return RTN_ERROR;
+            }
+            else if (tPollFds[1].revents & POLLHUP)
+            {
+                tPollFds[0].revents = 0;
+                return RTN_ERROR;
+            }
+            
+            tPollFds[1].revents = 0;
+        
+            iIdx = rand() % 10;
+            
+            memset((Packet *) &tPacket, 0x00, sizeof(tPacket));
+            strcpy(tPacket.sStr, sStr[iIdx]);
+            
+            if (send(*nSockFd, &tPacket, sizeof(tPacket), 0) == -1)
+                return RTN_ERROR;
+                
+            printf("%s\n", tPacket.sStr);
+        }
+    }
+}
