c5cdef8f039582beef4fbcef51361746e4154c72
Psyduck <31785341+C13H12N4O2@users.noreply.github.com>
2025-03-17 13:47:43 +0900

    데이터베이스 작업

--- C# ASP.NET Razor Pages Excercise/RazorPagesMovie/RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ C# ASP.NET Razor Pages Excercise/RazorPagesMovie/RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f35cfbf0d531496594be664a10d6aa25b4b2bcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b3f1df47bd85570f1635e8d8f70c1548b0c27a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
